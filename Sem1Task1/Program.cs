eae00812f1149d8ba467b8aa4ab30ed9c7fde83d
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-06 10:17:19 +0300

    add solution for 0,1,3

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+string Num1Line = Console.ReadLine();
+string Num2Line = Console.ReadLine();
+
+if(Num1Line!=null&&Num2Line!=null)
+{
+int num1=int.Parse(Num1Line);
+int num2=int.Parse(Num2Line);
+bool outResult = (num1==num2*num2);
+Console.WriteLine(outResult);
+}
---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+string lineA = Console.ReadLine();
+string [] dayWeek = new string[7];
+dayWeek[0]= "понедельник";
+dayWeek[1]= "вторник";
+dayWeek[2]= "среда";
+dayWeek[3]= "четверг";
+dayWeek[4]= "пятница";
+dayWeek[5]= "суббота";
+dayWeek[6]= "воскресенье";
+int A = int.Parse(lineA);
+Console.WriteLine(dayWeek[A-1]);
+
