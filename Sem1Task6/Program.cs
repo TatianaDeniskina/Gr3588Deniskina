3b43e2d4bbb8ec52d7b1f0972b4d2dfa96663643
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-06 11:46:55 +0300

    add solution for task 6

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+string Num = Console.ReadLine();
+
+int num=int.Parse(Num);
+
+int Result = num%2;
+if (Result==0)
+{
+    Console.WriteLine("Число "+num+" четное");
+}
+else
+{
+    Console.WriteLine("Число "+num+" нечетное");
+}
+
