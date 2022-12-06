b48ad6f3bdf670623ed58e249346b4474466e044
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-06 10:46:33 +0300

    dd solution for task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+string number1 = Console.ReadLine();
+string number2 = Console.ReadLine();
+
+int NUM1=int.Parse(number1);
+int NUM2=int.Parse(number2);
+
+if (NUM1>NUM2)
+{
+    Console.WriteLine("Первое число больше второго" +NUM1+">"+NUM2);
+}
+if (NUM1==NUM2)
+{
+    Console.WriteLine("Числа равны" +NUM1+"="+NUM2);
+}
+else
+    {
+        Console.WriteLine("Второе число больше первого" +NUM2+">"+NUM1);
+    }
