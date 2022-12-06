ff329b4bab21a0f330a7a9f7f90ba383c5982d9d
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-06 11:57:23 +0300

    add solution for task 8

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+string Num = Console.ReadLine();
+
+int num=int.Parse(Num);
+int Delitel = 2;
+string res = string.Empty;
+while (Delitel<num)
+{
+   res=res+Delitel+",";
+   Delitel=Delitel+2; 
+}
+Console.WriteLine(res);
