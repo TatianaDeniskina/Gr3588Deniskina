0c9d36b1fa766aa0232c6978078f17b3920cd0fe
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-06 10:24:58 +0300

    add solution 0

---
+++ Sem1task0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+string Num1Line = Console.ReadLine();
+string Num2Line = Console.ReadLine();
+
+if(Num1Line!=null&&Num2Line!=null)
+{
+int num1=int.Parse(Num1Line);
+int num2=int.Parse(Num2Line);
+int outResult=num1*num2;
+Console.WriteLine(outResult);
+}
+
