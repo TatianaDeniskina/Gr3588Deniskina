6f9aa310cd34012f346c99a1f42e7c5e12a96b2d
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-09 00:43:25 +0300

    add program for task 13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+int number = int.Parse(Console.ReadLine()); //вводим число
+Console.WriteLine (number); //выводим число
+
+if (number>99) // проверяем на наличие 3 цифры
+{
+    char[] digits = number.ToString().ToCharArray(); // заполняем массив цифрами введенного числа
+    Console.WriteLine(digits[2]); // выводим 3 цифру
+}
+else
+{
+    Console.WriteLine ("Число не имеет третьей цифры"); // выводим если нет 3 цифры
+}
+
