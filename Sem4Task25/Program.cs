922b0335dc220b75ca455be386442a2f2c788b3e
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-15 00:29:49 +0300

    add solution for task 25

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+int num = ReadData("Введите число: ");
+int pow = ReadData("Введите степень: ");
+int res = Pow(num,pow);
+
+PrintData(res);
+
+int ReadData(string msg)
+{
+   Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+ void PrintData(int res)
+ {
+    Console.WriteLine(res);
+   
+ }
+
+int Pow(int num, int pow)
+{
+    int res =1;
+    for (int i = 1;i<=pow;i++)
+    {
+        res=num*res;
+    }
+    return res;
+
+}
