cdeef1352786f60db07927d9985c38db5479114a
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-15 00:57:03 +0300

    add solution for task 27

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+int num = ReadData("Введите число: ");//вводим число
+int res = SumDigit(num);//считаем сумму цифр
+PrintData(res);//выводим результат
+
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+ void PrintData(int res)//метод вывода
+ {
+    Console.WriteLine(res);
+   
+ }
+int SumDigit(int num)//метод сложения цифр числа
+{
+    int res=0;
+    while (num>0)
+    {
+        res+=num%10;
+        num=num/10;
+    }
+    return res;
+}
