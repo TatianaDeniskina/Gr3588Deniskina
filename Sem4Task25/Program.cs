7cc6f3d3a96d52c01be2a60a855426e60781a39c
TatianaDeniskina <119048601+TatianaDeniskina@users.noreply.github.com>
2022-12-15 00:40:48 +0300

    Update Sem4Task25/Program.cs

--- Sem4Task25/Program.cs
+++ Sem4Task25/Program.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 
-int num = ReadData("Введите число: ");
-int pow = ReadData("Введите степень: ");
-int res = Pow(num,pow);
+int num = ReadData("Введите число: //вводим число
+int pow = ReadData("Введите степень: ");//вводим степень
+int res = Pow(num,pow);//считаем значение
 
-PrintData(res);
+PrintData(res);//выводим результат
 
-int ReadData(string msg)
+int ReadData(string msg)//метод ввода
 {
    Console.WriteLine(msg);
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
- void PrintData(int res)
+ void PrintData(int res)//метод вывода
  {
     Console.WriteLine(res);
    
  }
 
-int Pow(int num, int pow)
+int Pow(int num, int pow)//метод возведения в степень
 {
     int res =1;
     for (int i = 1;i<=pow;i++)
