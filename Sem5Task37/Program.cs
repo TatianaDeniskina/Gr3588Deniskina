4a93b806c509437cae640441b65323ae0751f995
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-20 15:59:41 +0300

    add solution for task 37

---
+++ Sem5Task37/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+int[] array = Arr(123,-100,100);
+PrintDArr(array);
+
+int[] res = ConvertArr(array);
+PrintDArr(res);
+
+int[] Arr(int len, int minValue, int maxValue)//метод заполнения массива рандoмными числами
+ {
+    int[] arr = new int [len];
+    Random random = new Random();
+    for (int i = 0; i<arr.Length;i++)
+    {
+        arr[i]= new Random().Next(minValue,maxValue+1);
+    }
+    return arr;
+ }
+ void PrintDArr(int[] arr) //печать одномерного массива
+ {
+    for(int i=0; i < arr.Length -1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length -1]);
+ }
+ int[] ConvertArr(int[]arr)
+ {
+    int[] outArr = new int[arr.Length/2];
+    for(int i=0; i<arr.Length/2; i++)
+    {
+        outArr[i]=arr[i]*arr[arr.Length-1-i];
+    }
+    return outArr;
+ }
