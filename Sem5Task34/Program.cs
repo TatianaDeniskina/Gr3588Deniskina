0b8618c8d6b17fdfa8c6c5d22438044cc441f244
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-20 16:15:59 +0300

    add solution for task 34

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+int[] array = Arr(123,100,999);
+PrintDArr(array);
+
+int res = CountEven(array);
+PrintData("Количество четных чисел в массиве: ", res);
+void PrintData(string res, int value)//метод вывода
+ {
+    Console.WriteLine(res+value);
+ }
+int[] Arr(int len, int minValue, int maxValue)//метод заполнения массива рандoмными числами
+ {
+    int[] arr = new int [len];
+    Random random = new Random();
+    for (int i = 0; i<arr.Length;i++)
+    {
+        arr[i]= new Random().Next(minValue,maxValue+1);
+    }
+    return arr;
+ }
+ void PrintDArr(int[] arr) //печать одномерного массива
+ {
+    for(int i=0; i < arr.Length -1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length -1]);
+ }
+int CountEven(int[]arr)
+{
+    int res =0;
+     for (int i=0; i < arr.Length; i++)
+    {
+        if(arr[i]%2==0)
+        res++;
+    }
+    return res;
+    
+}
