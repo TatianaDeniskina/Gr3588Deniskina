f554870ab277a98dd21310b5bba707f36b979480
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-21 14:30:31 +0300

    add solution for task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+int num = ReadData("Введите число ");
+
+PrintData("Результат: "+ DecToBin(num));
+
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string res)//метод вывода
+ {
+    Console.WriteLine(res);
+ }
+ string DecToBin(int num)
+ {
+    string outBin=String.Empty;
+    while (num>0)
+    {
+        outBin=(num%2)+outBin;
+        num=num/2;
+    }
+    return outBin;
+ }
