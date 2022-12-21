7503f7a860f0fcbe9a55568e004631b1eddc1e0a
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-21 14:18:14 +0300

    add solution for task 40

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+int a = ReadData("Введите первое число ");
+int b = ReadData("Введите второе число ");
+int c = ReadData("Введите третье число ");
+
+if(TriangleTest(a,b,c)) {PrintData("Треугольник существует ");}
+else {PrintData("Треугольник не существует ");}
+
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string res)//метод вывода
+ {
+    Console.WriteLine(res);
+ }
+ bool TriangleTest(int a,int b, int c)
+ {
+    return((a+b>c)&&(a+c>b)&&(b+c>a));
+ }
