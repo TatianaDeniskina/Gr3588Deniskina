450d2d1ab3071e2b9ce82585b6e03d044340a0b7
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-21 15:23:22 +0300

    add solution for task 41

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+int N = ReadData("Введите количество чисел: ");//вводим количество чисел
+PrintData("Результат: "+CountPosNum(N));//выводим результат
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+void PrintData(string res)//метод вывода
+ {
+    Console.WriteLine(res);
+ }
+ int CountPosNum(int N)//метод подсчета положительных чисел
+ {
+    int res = 0;
+    while(N>0)
+    {
+       if(ReadData("Введите число: ")>0) res++;
+
+        N=N-1;
+    }
+    return res;
+ }
