e18c9cd5ef8bcf086398c11f3b2bbb2197c35239
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-28 14:44:17 +0300

    add solution for task 53

---
+++ Sem8Task53/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+int row = ReadData("Введите количество строк: ");
+int colum = ReadData("Введите количество столбцов: ");
+int[,]arr2D =Fill2DArray(row, colum, 10, 99);
+Print2DArray(arr2D);
+Change2DArr(arr2D);
+
+
+Print2DArray(arr2D);
+
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+   return int.Parse(Console.ReadLine() ?? "0");
+}
+int[,] Fill2DArray(int countRow, int countColum, int topBorder, int downBorder)//метод генерации 2мерного массива
+{
+    Random rand= new Random();
+    int[,] arry2D = new int[countRow,countColum];
+    for (int i =0; i< countRow; i++)
+    {
+        for(int j =0; j< countColum; j++)
+        {
+            arry2D[i,j]=rand.Next(topBorder, downBorder+1);
+        }
+    }
+    return arry2D;
+}
+void Print2DArray(int[,] matrix)
+{
+    for (int i =0; i< matrix.GetLength(0); i++)
+    {
+        for(int j =0; j< matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+"    ".Substring(matrix[i,j].ToString().Length));
+        }
+        Console.WriteLine();
+    }
+}
+void Change2DArr(int[,] matrix)
+{
+    int temp = 0;
+    for (int i=0;i<matrix.GetLength(1);i++)
+    {
+        temp = matrix[0,i];
+        matrix[0,i]=matrix[matrix.GetLength(0)-1,i];
+
+    }
+}
