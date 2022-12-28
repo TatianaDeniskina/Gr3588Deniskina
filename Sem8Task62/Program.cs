7a9a9ad9bb32e689609af8ad88ffd4d6f48de564
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-28 16:11:06 +0300

    add solution for task 62

---
+++ Sem8Task62/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+int n = 4;//размер матрицы
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))//спирально заполняем массив
+{
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+Print2DArray(sqareMatrix);//выводим результат
+
+void Print2DArray(int[,] matrix)//метод вывода двумерного массива
+{
+    for (int i =0; i< matrix.GetLength(0); i++)
+    {
+        for(int j =0; j< matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+"    ".Substring(matrix[i,j].ToString().Length));
+        }
+        Console.WriteLine();
+    }
+}
