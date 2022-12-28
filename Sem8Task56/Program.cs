5ed75348180c10f2937c577772ed13ce3999dd79
Tatyana <makbuk@MacBook-Pro-MakBuk.local>
2022-12-28 15:25:27 +0300

    add solution for task 56

---
+++ Sem8Task56/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+int m = ReadData("Введите m: ");//вводим количество солбцов
+int n = ReadData("Введите n: ");//вводим количесество сторок
+int range = ReadData("Введите диапазон: от 1 до ");//вводим ограничение на предельное значение генерации
+ 
+int[,] arr= Fill2DArray(m,n,1,range);//генерируем массив
+Print2DArray(arr);//печатаем массив
+
+int minSumLine = 0;//задаем переменную для мининума
+int sumLine = SumLineElements(arr, 0);//задаем пременную для суммирования строк
+for (int i = 1; i < arr.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(arr, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)//метод нахождения суммы 
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int ReadData(string msg)//метод ввода
+{
+   Console.WriteLine(msg);
+   return int.Parse(Console.ReadLine() ?? "0");
+}
+int[,] Fill2DArray(int countRow, int countColum, int topBorder, int downBorder)//метод генерации 2мерного массива
+{
+    Random rand= new Random();
+    int[,] arry2D = new int[countRow,countColum];
+    for (int i =0; i< countRow; i++)
+    {
+        for(int j =0; j< countColum; j++)
+        {
+            arry2D[i,j]=rand.Next(topBorder, downBorder+1);
+        }
+    }
+    return arry2D;
+}
+
+void Print2DArray(int[,] matrix)//метод вывода двумерного массива
+{
+    for (int i =0; i< matrix.GetLength(0); i++)
+    {
+        for(int j =0; j< matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+"    ".Substring(matrix[i,j].ToString().Length));
+        }
+        Console.WriteLine();
+    }
+}
